25db443abeae55e4f0fe2e796e882d3ef25ce522
turjoy-dev <trinity.dc11@gmail.com>
2025-12-08 21:59:04 +0600

    Add hello_func and Hello_func to Func.py

---
+++ Func.py
@@ -0,0 +1,11 @@
+def hello_func():
+    print('Life is Okey : Yeeh')
+
+hello_func()
+hello_func()
+
+def Hello_func(Greeting):
+    return "{} Magic Life..".format(Greeting)
+
+print(Hello_func('Hi'))
+hello_func()
