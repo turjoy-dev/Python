526c6ade0a1677c266e0a6511756d87e73956d5b
turjoy-dev <trinity.dc11@gmail.com>
2025-12-09 00:44:06 +0600

    Add loop to count even and odd numbers in a range

---
+++ loop.py
@@ -0,0 +1,7 @@
+#Count How Many Even and Odd Numbers Are in a List\
+
+for num in range(1,50):
+   if(num % 2 == 0):
+         print(f"Even Number: {num}")
+   else:
+       print(f"odd Number : {num}")
